11f65a5602e50921d5f757c5270a9e9134c183d6
Florian Schulz <hanbei@hanbei.de>
2015-12-31 14:40:23 +0100

    Use flask webserver temporary

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn carl:app --log-file=-
+web: python runserver.py
--- runserver.py
+++ runserver.py
@@ -1,6 +1,9 @@
+import os
+
 from carl import app
 
 from carl.db import init_db
 
+port = int(os.environ.get('PORT', 33507))
 #init_db(app)
-app.run(debug=True)
+app.run(debug=True, port=port, host='0.0.0.0')
