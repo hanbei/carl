d254a6d0973514917207d054e8b725d80a10d874
Florian Schulz <hanbei@hanbei.de>
2015-12-31 14:45:55 +0100

    Fix db conection string

--- carl/__init__.py
+++ carl/__init__.py
@@ -1,7 +1,9 @@
+import os
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
-DATABASE_URL = 'postgres://postgres:passwd@172.17.0.2:5432/carl'
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://postgres:passwd@172.17.0.2:5432/carl')
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -11,6 +13,6 @@ app = Flask(__name__)
 Bootstrap(app)
 
 app.config.from_object(__name__)
-app.config.from_envvar('DATABASE_URL', silent=True)
+#app.config.from_envvar('DATABASE_URL')
 
 import carl.views
