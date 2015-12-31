6c677b401d45b4a8362d8f1e22d3fd2ba816069f
Florian Schulz <hanbei@hanbei.de>
2015-12-31 15:23:03 +0100

    Change user/passwd

--- carl/__init__.py
+++ carl/__init__.py
@@ -6,8 +6,8 @@ from flask_bootstrap import Bootstrap
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://postgres:passwd@172.17.0.2:5432/carl')
 DEBUG = True
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+USERNAME = 'nora'
+PASSWORD = 'hunger'
 
 app = Flask(__name__)
 Bootstrap(app)
