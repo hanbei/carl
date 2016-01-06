05982127f5763d42f3a3098d8fb06da1a9435784
Florian Schulz <hanbei@hanbei.de>
2016-01-06 21:35:30 +0100

    read config from env vars

--- carl/__init__.py
+++ carl/__init__.py
@@ -5,16 +5,17 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://postgres:passwd@172.17.0.2:5432/carl')
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'nora'
-PASSWORD = 'hunger'
+DEBUG = os.environ.get('DEBUG', True)
+SECRET_KEY = os.environ.get('SECRET_KEY', 'development key')
+USERNAME = os.environ.get('USERNAME', 'admin')
+PASSWORD = os.environ.get('PASSWORD', 'admin')
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config.from_object(__name__)
 
 from carl.navigation import nav
+
 nav.init_app(app)
 
 import carl.views
