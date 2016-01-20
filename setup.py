8829f5e462512ae05adf31459eb5b960be043934
Florian Schulz <hanbei@hanbei.de>
2016-01-20 21:37:52 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='carl',
+        version='1.0',
+        packages=['carl'],
+        url='',
+        license='Apache License, Version 2.0',
+        author='hanbei',
+        author_email='',
+        description='',
+        test_suite='nose.collector',
+        tests_require=['nose'],
+)
